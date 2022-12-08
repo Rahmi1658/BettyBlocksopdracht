342b4381ebcf041671615bb7a745162899248232
Rahmi tas <159159@student.horizoncollege.nl>
2022-12-08 23:57:11 +0100

    API added

--- betty-blocks/src/home/home.js
+++ betty-blocks/src/home/home.js
@@ -1,10 +1,26 @@
-const Home = () => {
-  return ( 
-    <div clasName='Home'>
-      <h2>Homepage</h2>
-      
+import React, { useState, useEffect } from 'react'; 
+const title = 'New cat fact';
+
+const App = () => {
+  const [data, setData] = useState({});
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://catfact.ninja/fact');
+      const json = await response.json();
+      setData(json);
+    }
+    fetchData();
+  }, []);
+  
+  return (
+    <div>
+      <h3>{title}</h3>
+      {Object.keys(data).map(key => (
+        <p key={key}>{data[key]}</p>
+      ))}
     </div>
-   );
+  );
 }
- 
-export default Home;
+
+export default App;
